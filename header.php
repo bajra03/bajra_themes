3baca8025556bd1226d3cf135ef6530ff59fe3eb
bajra <bajra.idabagus@karmagroup.com>
2019-10-29 14:24:55 +0800

    update favicon

--- header.php
+++ header.php
@@ -7,6 +7,10 @@
   <meta http-equiv="Pragma" content="no-cache" />
   <meta http-equiv="Expires" content="0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri();?>/assets/images/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri();?>/assets/images//favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri();?>/assets/images//favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
   <title>Putu Bajra</title>
   <?php
     wp_head();
