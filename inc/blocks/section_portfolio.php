e5d71e4292daa05f883b35b80416d3acb8150c93
bajra <bajra.idabagus@karmagroup.com>
2020-01-22 10:43:51 +0800

    update portfolio section

--- inc/blocks/section_portfolio.php
+++ inc/blocks/section_portfolio.php
@@ -13,36 +13,34 @@
     </div>
 
     <!-- Inner Section -->
-    <?php foreach ($portfolios as $key=>$portfolio) {?>
-
     <div class="inner-content">
       <div class="row portfolio-container">
-        <div class="col-md-12 more-portfolio">
-          <div class="row">
-            <div class="col-md-4">
-              <div class="portfolio-img">
-                <div class="portfolio-inner-img" style="background-image: url(<?php echo $portfolio['portfolio_image'] ?>);"></div>
+        <?php foreach ($portfolios as $key=>$portfolio) {?>
+          <div class="col-md-12 more-portfolio mb-3">
+            <div class="row">
+              <div class="col-md-4">
+                <div class="portfolio-img">
+                  <div class="portfolio-inner-img" style="background-image: url(<?php echo $portfolio['portfolio_image'] ?>);"></div>
+                </div>
+              </div>
+              <div class="col-md-8">
+                <div class="portfolio-details">
+                  <div class="portfolio-title">
+                    <h4><?php echo $portfolio['portfolio_title']?></h4>
+                  </div>
+                  <div class="portfolio-desc">
+                    <p><?php echo $portfolio['portfolio_description']?></p>
+                  </div>
+                  <div class="portfolio-footer">
+                    <a href="<?php echo $portfolio['portfolio_link']?>" class="btn btn-primary secondary-color" target="_blank">Visit</a>
+                  </div>
+                </div>          
               </div>
             </div>
-            <div class="col-md-8">
-              <div class="portfolio-details">
-                <div class="portfolio-title">
-                  <h4><?php echo $portfolio['portfolio_title']?></h4>
-                </div>
-                <div class="portfolio-desc">
-                  <p><?php echo $portfolio['portfolio_description']?></p>
-                </div>
-                <div class="portfolio-footer">
-                  <a href="<?php echo $portfolio['portfolio_link']?>" class="btn btn-primary secondary-color" target="_blank">Visit</a>
-                </div>
-              </div>          
-            </div>
-          </div>
-        </div>       
+          </div>       
+        <?php } ?>
       </div>      
     </div>
-
-    <?php } ?>
 
     <div id="load-more" class="load-more-wrapper text-center">
       <a href="#" class="btn btn-primary secondary-color">Load More</a>
